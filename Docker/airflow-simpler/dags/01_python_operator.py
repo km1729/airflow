9f58ba159747d53dcc18a29a1302812dbfef1f20
kmcin <kellymcintyre.dev@gmail.com>
2023-07-07 16:34:13 +1000

    feat: add get_age func

--- Docker/airflow-simpler/dags/01_python_operator.py
+++ Docker/airflow-simpler/dags/01_python_operator.py
@@ -11,16 +11,23 @@ default_args = {
 }
 
 #python code
-def greet(age, ti):
-    name = ti.xcom_pull(task_ids='get_name')
-    print(f"Hello world. I'm {name} and {age} years old")
+def greet(ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name= ti.xcom_pull(task_ids='get_name',key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    
+    print(f"Hello world. I'm {first_name} {last_name} and {age} years old")
 
-def get_name():
-    return 'Charlie'
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Tom')
+    ti.xcom_push(key='last_name', value='Friday')
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=19)
 
 with DAG(
     default_args=default_args,
-    dag_id='python_operator_v4',
+    dag_id='python_operator_v6',
     description='python operator test',
     start_date=datetime(2023,7,7,1),
     schedule_interval='@daily'
@@ -28,13 +35,17 @@ with DAG(
 ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet, #python function
-        op_kwargs={'age':15}
+        python_callable=greet #python function
+        # op_kwargs={'age':15}
     )
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name,
     )
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
 
     #task2를 먼저 실행해서 이름을 얻고, 그 결과를 task1에 전달한다
-    task2 >> task1
+    [task2,task3] >> task1
