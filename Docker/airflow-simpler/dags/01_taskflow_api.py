8b7f83c6ab387cf5f7434a0112c057bc220d764f
kmcin <kellymcintyre.dev@gmail.com>
2023-07-07 19:44:53 +1000

    feat: Add multiple_outputs dictionary

--- Docker/airflow-simpler/dags/01_taskflow_api.py
+++ Docker/airflow-simpler/dags/01_taskflow_api.py
@@ -7,26 +7,32 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-@dag(dag_id='taskflow_api_v01',
+@dag(dag_id='taskflow_api_v02',
      default_args=default_args,
      start_date=datetime(2023, 7, 7),
      schedule_interval='@daily')
 def hello_wrold_etl():
-    @task()
+
+    @task(multiple_outputs=True)
     def get_name():
-        return 'Kaycee'
+        return {
+            'first_name':'Kaycee',
+            'last_name':'Friday'
+        }
 
     @task()
     def get_age():
         return 19
     
     @task()
-    def greet(name, age):
-        print(f"Hello World! I am {name} and {age} years old")
+    def greet(first_name, last_name, age):
+        print(f"Hello World! I am {first_name} {last_name} and {age} years old")
 
 
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name=name,age=age)
+    greet(first_name=name_dict['first_name'],
+          last_name=name_dict['last_name'],
+          age=age)
 
 greet_dag = hello_wrold_etl()
