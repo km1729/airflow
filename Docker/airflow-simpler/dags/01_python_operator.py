9730e14de2d5f7a737c4b0ae6e811d179bb2fcee
kmcin <kellymcintyre.dev@gmail.com>
2023-07-07 15:14:56 +1000

    feat:Compose func using value to generate result

--- Docker/airflow-simpler/dags/01_python_operator.py
+++ Docker/airflow-simpler/dags/01_python_operator.py
@@ -11,12 +11,16 @@ default_args = {
 }
 
 #python code
-def greet(name, age):
+def greet(age, ti):
+    name = ti.xcom_pull(task_ids='get_name')
     print(f"Hello world. I'm {name} and {age} years old")
+
+def get_name():
+    return 'Charlie'
 
 with DAG(
     default_args=default_args,
-    dag_id='python_operator_v2',
+    dag_id='python_operator_v4',
     description='python operator test',
     start_date=datetime(2023,7,7,1),
     schedule_interval='@daily'
@@ -25,5 +29,12 @@ with DAG(
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet, #python function
-        op_kwargs={'name':'tom', 'age':15}
+        op_kwargs={'age':15}
     )
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name,
+    )
+
+    #task2를 먼저 실행해서 이름을 얻고, 그 결과를 task1에 전달한다
+    task2 >> task1
